2022d17d6a308bfa183fd70d5e0dd5917bd4b04e
Sivaparvathi <sivaparvathi.tatisetti4@gmail.com>
2026-06-09 00:46:49 +0530

    Modified

--- src/Helloworld.java
+++ src/Helloworld.java
@@ -3,6 +3,6 @@ import java.net.SocketOption;
 public class Helloworld {
     public static void main(String[] args)
     {
-        System.out.println("HelloWorld");
+        System.out.println("Hello World");
     }
 }

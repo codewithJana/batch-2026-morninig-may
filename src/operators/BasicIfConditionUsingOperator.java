bbe5ff9775b4dff93f625493567d86365f3efd50
Ritheshkumar2 <ritheshbrahmanapally@gmail.com>
2026-06-11 17:46:49 +0530

    message

---
+++ src/operators/BasicIfConditionUsingOperator.java
@@ -0,0 +1,23 @@
+package operators;
+
+import java.util.Scanner;
+
+public class BasicIfConditionUsingOperator {
+
+	public static void main(String[] args) {
+		test();
+
+	}
+
+	private static void test() {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the number:");
+		int n=sc.nextInt();
+		
+		if(18>n) {
+			System.out.print("n is greater than 18");
+		}
+		
+	}
+
+}

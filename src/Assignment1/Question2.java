bb4f994891e44c3c49dcebd7e9bff8d44a159cbb
Nagesh <rakshitha.n.nagesh@accenture.com>
2026-06-20 21:15:06 +0530

    Question3

---
+++ src/Assignment1/Question2.java
@@ -0,0 +1,16 @@
+package Assignment1;
+
+public class Question2 {
+    static void main(String[] args) {
+
+        int sum = 0;
+        int i=1;
+        while(i <= 20) {
+            sum=sum+i;
+            i++;
+
+        }
+        System.out.println("Sum of 1 to 20 = "+sum);
+    }
+
+}
---
+++ src/Assignment1/Question3.java
@@ -0,0 +1,15 @@
+package Assignment1;
+
+public class Question3 {
+
+    static void main(String[] args) {
+
+        int[] marks={85, 92, 67, 78, 55, 90};
+
+        for(int i=0;i< marks.length;i++){
+            System.out.println("Index 0 : "+marks[i]);
+        }
+        System.out.println("Total Elements : " +marks.length);
+    }
+
+}

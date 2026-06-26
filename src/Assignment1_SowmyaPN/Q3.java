4b7044ea97ce1498bd5093f3a8007aef45f90a1f
SOWMYA-PN <https://github.com/SOWMYA-PN/JavaPractice_PNS.com>
2026-06-26 10:12:26 +0530

    Assignment1 Question3

---
+++ src/Assignment1_SowmyaPN/Q3.java
@@ -0,0 +1,11 @@
+package Assignment1_SowmyaPN;
+
+public class Q3 {
+    public static void main(String[] args) {
+        int[] marks = {85, 92, 67, 78, 55, 90};
+        for (int i =0 ; i < marks.length; i++ ){
+            System.out.println("Index["+ i + "]" + ":" + marks[i]);
+        }
+        System.out.println("Total elements in the array is " + marks.length);
+    }
+}
